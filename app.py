1e4d62f563b50818aebf8381bf405c5e99085bc2
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-08-22 02:06:51 -0400

    startup page

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    today = date.today()
+    number = 1
+    curDate = today.strftime("%B %d, %Y")
+    return render_template("startup.html", date=curDate, number=number)
--- static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var text = document.getElementById('text');
-    var typewriter = new Typewriter(text, {
-        cursor: '<span style="color: #ffffff;">|</span>',
-        delay: 150
-    });
-    typewriter.start()
-    typewriter.typeString('Welcome. Have you played before?')
-});
