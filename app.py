7fcb0178c6ddf573a7bcb2a94e54603a14cfc29c
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-08-23 01:40:57 -0400

    virtual keyboard

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ Session(app)
 today = date.today()
 number = 1
 curDate = today.strftime("%B %d, %Y")
+TESTCRYPTOGRAM = 'GDS FGEGS XC YZRRSFXGE GEQSF ZGF REYS CKXY E REGZLS HEQXGE IXKH CXK "FQV-GZRGSH IEGSK."'
 
 @app.route("/")
 def index():
     if not session.get("returning"):
         return redirect("/welcome")
-    return render_template("index.html", date=curDate, number=number)
+    return render_template("index.html", date=curDate, number=number, cryptogramText=TESTCRYPTOGRAM)
 
 @app.route("/welcome", methods=["GET", "POST"])
 def new():
     if request.method == "POST":
         session["returning"] = request.form.get("newUser")
         return redirect("/")
-    return render_template("startup.html", date=curDate, number=number)
+    return render_template("welcome.html", date=curDate, number=number)
