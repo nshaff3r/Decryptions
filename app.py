1eabe56b289080d3923522906bbd7dca112a5cdf
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-08-24 18:14:03 -0400

    first working draft

--- app.py
+++ app.py
@@ -4,23 +4,30 @@ from datetime import date, timedelta
 
 app = Flask(__name__)
 
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30000)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 today = date.today()
 number = 1
 curDate = today.strftime("%B %d, %Y")
-TESTCRYPTOGRAM = 'GDS FGEGS XC YZRRSFXGE GEQSF ZGF REYS CKXY E REGZLS HEQXGE IXKH CXK "FQV-GZRGSH IEGSK."'
-TESTSOLUTION =  'THE STATE OF MINNESOTA TAKES ITS NAME FROM A NATIVE DAKOTA WORD FOR "SKY-TINTED WATER."'
+testCryptogram = 'GDS FGEGS XC YZRRSFXGE GEQSF ZGF REYS CKXY E REGZLS HEQXGE IXKH CXK "FQV-GZRGSH IEGSK."'
+testSolution =  'THE STATE OF MINNESOTA TAKES ITS NAME FROM A NATIVE DAKOTA WORD FOR "SKY-TINTED WATER."'
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     print("hello, world!")
     if not session.get("returning"):
         return redirect("/welcome")
-
-    return render_template("index.html", date=curDate, number=number, lives=5, cryptogramText=TESTCRYPTOGRAM)
+    if not session.get("lives"):
+        session["lives"] = 5
+    if not session.get("replaced"):
+        session["replaced"] = []
+    if not session.get("failed"):
+        session["failed"] = []
+    return render_template("index.html", date=curDate, number=number,
+                           lives=session["lives"], cryptogramText=testCryptogram,
+                           replaced=session["replaced"], failed=session["failed"])
 
 @app.route("/welcome", methods=["GET", "POST"])
 def new():
@@ -32,7 +39,10 @@ def new():
 @app.route("/api", methods=["POST"])
 def api():
     data = request.json
-    if TESTCRYPTOGRAM.find(data["old"]) == TESTSOLUTION.find(data["new"]):
-        return jsonify({'message': 'correct'}), 200
+    if testCryptogram.find(data["old"]) == testSolution.find(data["new"]):
+         session["replaced"].append(data["old"] + data["new"])
+         return jsonify({'message': 'correct', 'lives': session["lives"]}), 200
     else:
-        return jsonify({'message': 'wrong'}), 200
+        session["lives"] -= 1
+        session["failed"].append(data["old"] + data["new"])
+        return jsonify({'message': 'wrong', 'lives': session["lives"]}), 200
