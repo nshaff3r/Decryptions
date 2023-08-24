70d96c47f95eed8845e2964381f07cde6b84e2ac
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-08-24 00:53:09 -0400

    added lives, added interactivity and correct testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from datetime import date, timedelta
 
@@ -12,12 +12,15 @@ today = date.today()
 number = 1
 curDate = today.strftime("%B %d, %Y")
 TESTCRYPTOGRAM = 'GDS FGEGS XC YZRRSFXGE GEQSF ZGF REYS CKXY E REGZLS HEQXGE IXKH CXK "FQV-GZRGSH IEGSK."'
+TESTSOLUTION =  'THE STATE OF MINNESOTA TAKES ITS NAME FROM A NATIVE DAKOTA WORD FOR "SKY-TINTED WATER."'
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print("hello, world!")
     if not session.get("returning"):
         return redirect("/welcome")
-    return render_template("index.html", date=curDate, number=number, cryptogramText=TESTCRYPTOGRAM)
+
+    return render_template("index.html", date=curDate, number=number, lives=5, cryptogramText=TESTCRYPTOGRAM)
 
 @app.route("/welcome", methods=["GET", "POST"])
 def new():
@@ -25,3 +28,11 @@ def new():
         session["returning"] = request.form.get("newUser")
         return redirect("/")
     return render_template("welcome.html", date=curDate, number=number)
+
+@app.route("/api", methods=["POST"])
+def api():
+    data = request.json
+    if TESTCRYPTOGRAM.find(data["old"]) == TESTSOLUTION.find(data["new"]):
+        return jsonify({'message': 'correct'}), 200
+    else:
+        return jsonify({'message': 'wrong'}), 200
