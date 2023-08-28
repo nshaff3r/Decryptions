e64a18fb6468499077bb0bd3090fae4733c69f7e
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2023-08-28 01:01:35 -0400

    bug fixes, complete page with copy feature

--- app.py
+++ app.py
@@ -11,9 +11,20 @@ Session(app)
 today = date.today()
 number = 1
 curDate = today.strftime("%B %d, %Y")
-testCryptogram = 'GDS FGEGS XC YZRRSFXGE GEQSF ZGF REYS CKXY E REGZLS HEQXGE IXKH CXK "FQV-GZRGSH IEGSK."'
-testSolution =  'THE STATE OF MINNESOTA TAKES ITS NAME FROM A NATIVE DAKOTA WORD FOR "SKY-TINTED WATER."'
-count = 16
+slashedDate = today.strftime("%m/%d")
+if (slashedDate[:2] != "10"):
+    slashedDate = slashedDate.replace("0", "")
+
+# testCryptogram = 'ZY PKL XSH XC XNNAH X MXP, ZS QKLAM SXIH KUHE SQHCSP PHXEV SK SEP XAA SRH ICKQC UXEZHSZHV.'
+# testSolution =  'IF YOU ATE AN APPLE A DAY, IT WOULD TAKE OVER TWENTY YEARS TO TRY ALL THE KNOWN VARIETIES. '
+testCryptogram = "Q"
+testSolution = "A"
+count = 0
+alpha = "ABCDEFGHIJKLMNOPQRSTUVQXYZ"
+for letter in alpha:
+    if letter not in testSolution:
+        count += 1
+count = 26 - count
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -26,16 +37,30 @@ def index():
         session["replaced"] = []
     if not session.get("failed"):
         session["failed"] = []
-    return render_template("index.html", date=curDate, number=number,
+    if len(session["replaced"]) == count:
+        return redirect("/complete")
+    return render_template("index.html", date="June 26,2023", number=number,
                            lives=session["lives"], cryptogramText=testCryptogram,
                            replaced=session["replaced"], failed=session["failed"])
 
 @app.route("/welcome", methods=["GET", "POST"])
-def new():
+def welcome():
     if request.method == "POST":
         session["returning"] = request.form.get("newUser")
         return redirect("/")
     return render_template("welcome.html", date=curDate, number=number)
+
+@app.route("/complete")
+def complete():
+    if not session.get("replaced"):
+        return redirect("/")
+    if len(session["replaced"]) == count:
+        return render_template("complete.html", date=curDate, number=number,
+                           lives=session["lives"], solvedCryptogram=testSolution,
+                           dateDashed=slashedDate, attempts=5 - session["lives"])
+
+    else:
+        return redirect("/")
 
 @app.route("/api", methods=["POST"])
 def api():
