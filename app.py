1d5534da924a0cf369a1a951af6217595c7546d5
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2023-08-29 00:15:59 -0400

    very sexy stats page

--- app.py
+++ app.py
@@ -32,13 +32,32 @@ def index():
     if not session.get("returning"):
         return redirect("/welcome")
     if not session.get("lives"):
-        session["lives"] = 5
+        session["lives"] = 2
     if not session.get("replaced"):
         session["replaced"] = []
     if not session.get("failed"):
         session["failed"] = []
-    if len(session["replaced"]) == count:
+    if not session.get("finished"):
+        session["finished"] = False
+    if session["finished"]:
         return redirect("/complete")
+    if not session.get("history"):
+        session["history"] = {
+            "games": 0,
+            "solved": 0,
+            "streak": 0,
+            "maxStreak": 0,
+            "avgLives": 0,
+            "closeCalls": 0,
+            "lives": {
+                0: 0,
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0
+            }
+        }
     return render_template("index.html", date="June 26,2023", number=number,
                            lives=session["lives"], cryptogramText=testCryptogram,
                            replaced=session["replaced"], failed=session["failed"])
@@ -52,26 +71,54 @@ def welcome():
 
 @app.route("/complete")
 def complete():
-    if not session.get("replaced"):
+    if not session.get("finished"):
         return redirect("/")
-    if len(session["replaced"]) == count:
-        return render_template("complete.html", date=curDate, number=number,
-                           lives=session["lives"], solvedCryptogram=testSolution,
-                           dateDashed=slashedDate, attempts=5 - session["lives"])
+    if session["finished"]:
+        session["history"]["avgLives"] = (session["history"]["streak"] * \
+            session["history"]["games"] + session["lives"]) / (session["history"]["games"] + 1)
+        session["history"]["games"] += 1
+        if session["lives"] <= 0:
+            win = False
+            session["history"]["streak"] = 0
+        else:
+            win = True
+            session["history"]["solved"] += 1
+            session["history"]["streak"] += 1
+            if session["history"]["streak"] > session["history"]["maxStreak"]:
+                session["history"]["maxStreak"] = session["history"]["streak"]
+            if session["lives"] == 1:
+                session["history"]["closeCalls"] += 1
+        return render_template("complete.html", date=curDate, number=number, solvedCryptogram=testSolution,
+                           dateDashed=slashedDate, attempts=5 - session["lives"], status=win)
 
     else:
         return redirect("/")
+
+@app.route("/stats")
+def stats():
+    if not session.get("history"):
+        session["history"] = {
+            "games": 10,
+            "solved": 7,
+            "streak": 3,
+            "maxStreak": 3,
+            "avgLives": 1.667,
+            "closeCalls": 1,
+            "lives": [3, 1, 4, 1, 1, 0]
+        }
+    return render_template("stats.html", date=curDate, number=number, data=session["history"])
 
 @app.route("/api", methods=["POST"])
 def api():
     data = request.json
     if testCryptogram.find(data["old"]) == testSolution.find(data["new"]):
-         complete = False
          session["replaced"].append(data["old"] + data["new"])
          if len(session["replaced"]) == count:
-             complete = True
-         return jsonify({'message': 'correct', 'lives': session["lives"], 'complete': complete}), 200
+             session["finished"] = True
+         return jsonify({'message': 'correct', 'lives': session["lives"], 'complete': session["finished"]}), 200
     else:
         session["lives"] -= 1
         session["failed"].append(data["old"] + data["new"])
-        return jsonify({'message': 'wrong', 'lives': session["lives"], 'complete': False}), 200
+        if (int(session["lives"]) == 0):
+            session["finished"] = True
+        return jsonify({'message': 'wrong', 'lives': session["lives"], 'complete': session["finished"]}), 200
