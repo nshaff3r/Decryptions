d40ee145a3dbc23221c45662fc72fd217d2af721
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2023-08-31 11:23:53 -0400

    bug fixes

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ if (slashedDate[:2] != "10"):
 
 # testCryptogram = 'ZY PKL XSH XC XNNAH X MXP, ZS QKLAM SXIH KUHE SQHCSP PHXEV SK SEP XAA SRH ICKQC UXEZHSZHV.'
 # testSolution =  'IF YOU ATE AN APPLE A DAY, IT WOULD TAKE OVER TWENTY YEARS TO TRY ALL THE KNOWN VARIETIES. '
-testCryptogram = "ABCDEFG HIJK LMNOP QRSTUV WXYZ"
-testSolution = "A"
+testCryptogram = "QQQ QQQ QQ Q Q QQQ QQ QQ QQ Q Q QQQQQQ QQ QQQ QQQQQQQ QQQ QQQ QQQ QQQQQ QQQQ QQQQQ QQQ QQ QQQQQ QQ"
+testSolution = "AAA AAA AA A A AAA AA AA AA A A AAAAAA AA AAA AAAAAAA AAA AAA AAA AAAAA AAAA AAAAA AAA AA AAAAA AA"
 count = 0
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for letter in alpha:
@@ -32,7 +32,7 @@ def index():
     if not session.get("returning"):
         return redirect("/welcome")
     if not session.get("lives"):
-        session["lives"] = 2
+        session["lives"] = 5
     if not session.get("replaced"):
         session["replaced"] = []
     if not session.get("failed"):
@@ -49,16 +49,9 @@ def index():
             "maxStreak": 0,
             "avgLives": 0,
             "closeCalls": 0,
-            "lives": {
-                0: 0,
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0,
-                5: 0
-            }
+            "lives": [0, 0, 0, 0, 0, 0]
         }
-    return render_template("index.html", date="June 26,2023", number=number,
+    return render_template("index.html", date=curDate, number=number,
                            lives=session["lives"], cryptogramText=testCryptogram,
                            replaced=session["replaced"], failed=session["failed"])
 
@@ -73,23 +66,28 @@ def welcome():
 def complete():
     if not session.get("finished"):
         return redirect("/")
-    if session["finished"]:
-        session["history"]["avgLives"] = (session["history"]["streak"] * \
-            session["history"]["games"] + session["lives"]) / (session["history"]["games"] + 1)
-        session["history"]["games"] += 1
-        if session["lives"] <= 0:
-            win = False
-            session["history"]["streak"] = 0
-        else:
-            win = True
-            session["history"]["solved"] += 1
-            session["history"]["streak"] += 1
-            if session["history"]["streak"] > session["history"]["maxStreak"]:
-                session["history"]["maxStreak"] = session["history"]["streak"]
-            if session["lives"] == 1:
-                session["history"]["closeCalls"] += 1
-        return render_template("complete.html", date=curDate, number=number, solvedCryptogram=testSolution,
-                           dateDashed=slashedDate, attempts=5 - session["lives"], status=win)
+    if not session.get("stats"):
+        session["stats"] = True
+        if session["finished"]:
+            if session["lives"] < 0:
+                session["lives"] = 0
+            session["history"]["avgLives"] = (session["history"]["streak"] * \
+                session["history"]["games"] + session["lives"]) / (session["history"]["games"] + 1)
+            session["history"]["games"] += 1
+            session["history"]["lives"][session["lives"]] += 1
+            if session["lives"] == 0:
+                win = False
+                session["history"]["streak"] = 0
+            else:
+                win = True
+                session["history"]["solved"] += 1
+                session["history"]["streak"] += 1
+                if session["history"]["streak"] > session["history"]["maxStreak"]:
+                    session["history"]["maxStreak"] = session["history"]["streak"]
+                if session["lives"] == 1:
+                    session["history"]["closeCalls"] += 1
+            return render_template("complete.html", date=curDate, number=number, solvedCryptogram=testSolution,
+                            dateDashed=slashedDate, attempts=5 - session["lives"], status=win)
 
     else:
         return redirect("/")
@@ -98,13 +96,13 @@ def complete():
 def stats():
     if not session.get("history"):
         session["history"] = {
-            "games": 10,
-            "solved": 7,
-            "streak": 3,
-            "maxStreak": 3,
-            "avgLives": 1.667,
-            "closeCalls": 188,
-            "lives": [3, 1, 4, 1, 1, 0]
+            "games": 0,
+            "solved": 0,
+            "streak": 0,
+            "maxStreak": 0,
+            "avgLives": 0,
+            "closeCalls": 0,
+            "lives": [0, 0, 0, 0, 0, 0]
         }
     return render_template("stats.html", date=curDate, number=number, data=session["history"])
 
